9ddf627fff7937f71a02545c60f34f6dbc47b37f
Kate <kit.ty.kate@disroot.org>
2020-10-07 18:49:33 +0100

    Encapsulate bemenu

--- src/main.ml
+++ src/main.ml
@@ -1,41 +1,69 @@
-type dirname = string
-type filename = string
+type dirname = Fpath.t
+type filename = Fpath.t
+type cmd = string
 
 module Filenames = Set.Make (String)
 
-let (//) = Filename.concat
+let (>>=) = Rresult.(>>=)
+let (>>|) = Rresult.(>>|)
+let (//) = Fpath.(/)
 
-let get_path () : dirname list =
+let path : dirname list =
   Sys.getenv_opt "PATH" |>
   Option.map (String.split_on_char ':') |>
-  Option.value ~default:[]
+  Option.value ~default:[] |>
+  List.map Fpath.v
 
-let get_executables (path : dirname list) : Filenames.t =
-  List.fold_left (fun acc dirname ->
-    try
-      let dir = Unix.opendir dirname in
-      let rec loop acc =
-        try
-          let filename = Unix.readdir dir in
-          if filename.[0] = '.' then begin
-            loop acc
-          end else begin
-            let fullname = dirname // filename in
-            Unix.access fullname [Unix.X_OK];
-            loop (Filenames.add filename acc)
-          end
-        with
-        | Unix.Unix_error _ -> loop acc
-        | End_of_file -> acc
-      in
-      loop acc
-    with
-    | Unix.Unix_error _ -> acc
-  ) Filenames.empty path
+let history_log : (filename, [> Rresult.R.msg ]) result =
+  Bos.OS.Dir.user () >>| fun home ->
+  home // ".config" // "sway" // "cmd-history"
+
+let history_limit = 50
+
+let history : cmd list =
+  Rresult.R.ignore_error ~use:(fun _ -> []) begin
+    history_log >>= fun history_log ->
+    Bos.OS.File.read_lines history_log
+  end
+
+let get_executables (acc : Filenames.t) : Filenames.t =
+  List.fold_left (fun acc dir ->
+    Bos.OS.Dir.fold_contents ~dotfiles:false ~elements:`Files ~traverse:`None (fun fullname acc ->
+      try
+        Unix.access (Fpath.to_string fullname) [Unix.X_OK];
+        Filenames.add (Fpath.filename fullname) acc
+      with
+      | Unix.Unix_error _ -> acc
+    ) acc dir |>
+    Rresult.R.ignore_error ~use:(fun _ -> acc)
+  ) acc path
+
+let exec_cmd (execs : Filenames.t) : (cmd * Bos.OS.Cmd.run_status, Rresult.R.msg) result =
+  let execs =
+    execs |>
+    Filenames.elements |>
+    String.concat "\n"
+  in
+  Bos.OS.Cmd.in_string execs |>
+  Bos.OS.Cmd.run_io Bos.Cmd.(v "env" % "LD_LIBRARY_PATH=/usr/local/lib" % "BEMENU_BACKEND=wayland" % "bemenu" % "-l" % "5") |>
+  Bos.OS.Cmd.out_string
+
+let save_cmd (result : (cmd * Bos.OS.Cmd.run_status, Rresult.R.msg) result) : unit =
+  Rresult.R.ignore_error ~use:(fun _ -> ()) begin
+    history_log >>= fun history_log ->
+    result >>= function
+    | cmd, (_, `Exited 0) ->
+        history @ [cmd] |>
+        List.filteri (fun i _ -> i <= history_limit) |>
+        List.rev |>
+        Bos.OS.File.write_lines history_log
+    | _ ->
+        Ok ()
+  end
 
 let () =
-  get_path () |>
+  history |>
+  Filenames.of_list |>
   get_executables |>
-  Filenames.elements |>
-  String.concat "\n" |>
-  print_endline
+  exec_cmd |>
+  save_cmd
