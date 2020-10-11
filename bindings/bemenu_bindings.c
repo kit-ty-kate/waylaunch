8861cef9f0eae945beac393a646288c4658404c9
Kate <kit.ty.kate@disroot.org>
2020-10-11 22:20:03 +0100

    C code cleanup

--- bindings/bemenu_bindings.c
+++ bindings/bemenu_bindings.c
@@ -4,33 +4,26 @@
 #include "bemenu/bemenu.h"
 
 const char *run_menu(struct bm_menu *menu) {
-    uint32_t unicode;
-    enum bm_key key;
-    enum bm_run_result status = BM_RUN_RESULT_RUNNING;
-
     bm_menu_set_lines(menu, 5);
     bm_menu_set_filter_mode(menu, BM_FILTER_MODE_DMENU);
     bm_menu_set_title(menu, "waylaunch");
     bm_menu_set_panel_overlap(menu, true);
     bm_menu_grab_keyboard(menu, true);
 
-    do {
+    enum bm_run_result status = BM_RUN_RESULT_RUNNING;
+    while (status == BM_RUN_RESULT_RUNNING) {
         bm_menu_render(menu);
-        key = bm_menu_poll_key(menu, &unicode);
-    } while ((status = bm_menu_run_with_key(menu, key, unicode)) == BM_RUN_RESULT_RUNNING);
+        uint32_t unicode = 0;
+        enum bm_key key = bm_menu_poll_key(menu, &unicode);
+        status = bm_menu_run_with_key(menu, key, unicode);
+    }
 
-    switch (status) {
-    case BM_RUN_RESULT_SELECTED:
-        {
-            uint32_t count = 0;
-            struct bm_item **items = bm_menu_get_selected_items(menu, &count);
-            if (count > 0) {
-                return bm_item_get_text(items[0]);
-            }
+    if (status == BM_RUN_RESULT_SELECTED) {
+        uint32_t count = 0;
+        struct bm_item **items = bm_menu_get_selected_items(menu, &count);
+        if (count > 0) {
+            return bm_item_get_text(items[0]);
         }
-        break;
-    default:
-        break;
     }
 
     return "";
@@ -42,8 +35,8 @@ value bmenu_binding(value l) {
 
     bm_init();
 
-    struct bm_menu *menu;
-    if ((menu = bm_menu_new()) == NULL) {
+    struct bm_menu *menu = menu = bm_menu_new();
+    if (menu == NULL) {
         r = caml_copy_string("");
         CAMLreturn (r);
     }
